f389666b45ece4d1c8b031ee3511b36d31e62a14
庄野舞子 <shounomaiko@shounomaikonoMacBook-puro.local>
2020-02-03 09:12:14 -0500

    Implemented Users and login namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root to: "posts#index"
   devise_for :users
-  resources :posts
+  resources :posts, only: [:index, :show]
+
+  namespace :login do
+    resources :posts
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_30_174313) do
+ActiveRecord::Schema.define(version: 2020_01_31_151630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2020_01_30_174313) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
